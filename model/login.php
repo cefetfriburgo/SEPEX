803c46f298066422e42d467d1b5cc7a23fe67bae
pedrohenriqueabib <gabrielbuquerb@gmail.com>
2019-08-08 17:27:21 -0300

    sistema de login ver

--- model/login.php
+++ model/login.php
@@ -19,7 +19,7 @@
 
 			/* Aplica a validação ao usuário e senha passados, utilizando as regras de négocio especificas para ele. */
 			
-				if(substr($array[1], -11) !="cefet-rj.br"){
+				if(substr($array[1], -11) !="cefet-rj.br"){// verifica os ultimos 11 caracteres
 					
 					header('location:./../view/admin/login.php');//	return 'Digite o usuário corretamente';
 
