f92c94807372c4abdca73fe4335dc89e4402efc8
anajuliasoares <anajuliacardososoares46@gmail.com>
2019-08-20 18:09:23 -0300

    atualizar_evento.php nao esta concluido

--- controller/atualizar_evento.php
+++ controller/atualizar_evento.php
@@ -18,9 +18,51 @@
         }
 
         public function atualizar($id, $nome, $ano, $semestre, $data_inicio, $hora_inicio, $data_fim, $hora_fim){
+           
+            $erro = false;
             if(!isset($id) || !isset($nome) || !isset($ano) || !isset($semestre) || !isset($data_inicio) || !isset($hora_inicio) || !isset($data_fim) || !isset($hora_fim)){
                 header('location: ./../view/admin/evento/editar.php');
-            }else{
+            }
+
+            if (!isset($_POST) || empty($_POST)){
+                $erro = 'Nada foi postado';
+            }
+           
+            foreach ($_POST as $campo => $valor){
+                $campo = trim(strip_tags($valor));
+                if (empty($valor)){
+                    $erro = 'Existem campos em branco';
+                }
+            }
+
+            if ((!isset($nome) || is_numeric($nome)) && !$erro){
+                $erro = 'Nome não deve ser numérico';
+            }
+
+            if ((!isset($ano) || !is_numeric($ano)) && !$erro){
+                $erro = 'Ano deve ser um valor numérico';
+            }
+
+            function ValidaData($data_inicio){
+                $data = explode("-","$data_inicio"); 
+                $d = $data[2];
+                $m = $data[1];
+                $y = $data[0];
+             
+                $res = checkdate($m,$d,$y);
+                if ($res == 0){
+                   echo "Data inválida";
+                }
+
+                if ($y < 2019){
+                    echo "Ano da data inválido";
+                }
+            }
+            //validacao nao esta funcionando
+                       
+            if ($erro) {
+                echo $erro;
+            } else{
                 $this->evento->atualizarEvento($id, $nome, $ano, $semestre, $data_inicio, $hora_inicio, $data_fim, $hora_fim);
                 header('location: ./../view/admin/evento/listar.php');
             }
@@ -28,7 +70,5 @@
     }
 
     $ctrlEvento = new ControllerEvento();
-    $ctrlEvento->atualizar($id, $nome, $ano, $semestre, $data_inicio, $hora_inicio, $data_fim, $hora_fim);
-    
-   
+    $ctrlEvento->atualizar($id, $nome, $ano, $semestre, $data_inicio, $hora_inicio, $data_fim, $hora_fim);  
 ?>
