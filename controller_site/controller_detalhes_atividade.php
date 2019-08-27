9f35f0e665a2a58184a0e238844cfe96ba5e2f10
pedrohenriqueabib <gabrielbuquerb@gmail.com>
2019-08-27 17:57:45 -0300

    teste2

--- controller_site/controller_detalhes_atividade.php
+++ controller_site/controller_detalhes_atividade.php
@@ -9,16 +9,33 @@
         }
 
         public function detalhesAtividade($id){
-            return $this->detalhesAtividade->exibirDetalhesAtividade();
-            // $p = $this->detalhesAtividade->exibirDetalhesAtividade($id);
+            return $this->detalhesAtividade->exibirDetalhesAtividade($id);
+            //$p = $this->detalhesAtividade->exibirDetalhesAtividade($id);
+
+            // foreach($p as $i => $pb){
+            //     echo $pb['nome_atividade'] . ' - '. $pb['idEvento'] . ' - '. $pb['idTipoAtividade'] 
+            //     . ' - '. $pb['descricao'] . ' - '. $pb['data'] . ' - '. $pb['hora_inicio']
+            //     . ' - '. $pb['hora_fim'] . ' - '. $pb['capacidade'] . '<br>';
+            // }
+            
+            //var_dump($p);
+        }
+
+        public function colaboradoresAtividade($id){
+            return $this->detalhesAtividade->exibirColaboradoresAtividade($id);
+            //$p = $this->detalhesAtividade->exibirColaboradoresAtividade($id);
 
             // foreach($p as $pb){
-            //     echo $pb['nome'] . '-->' . $pb['nome_atividade'] . '<br>';
+            //     echo $pb['nome'] . '<br>';
             // }
+            
+            //var_dump($p);
         }
     }
 
-    // $c = new ControllerDetalhesAtividade();
-    // $c->detalhesAtividade(4);
+    $c = new ControllerDetalhesAtividade();
+    $c->detalhesAtividade(4);
+    $c->colaboradoresAtividade(4);
+    
 ?>
 
