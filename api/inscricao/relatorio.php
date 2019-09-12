a14bbdf1ec7cbf307ac1c49ed9920d55450ce544
pedrohenriqueabib <pedrohenriqueabib@gmail.com>
2019-09-12 15:08:46 -0300

    correcao relatorio

--- api/inscricao/relatorio.php
+++ api/inscricao/relatorio.php
@@ -1,26 +1,29 @@
 <?php 
-header("Content-Type: application/json")
+header("Content-Type: application/json");
 require_once "./../../controller_site/controller_relatorio.php";
 
+
 $email = $_POST['email'];
+
 $relatorio = new ControllerRelatorio();
 $registros = $relatorio->relatorio($email);
 
 $atividade = [];
 
-foreach($registros as $registro){
-    array_push($atividade, array(
-        "nome_atividade" => $l['nome_atividade'],
-        "data" => date('d-m-Y', strtotime($registro['data'])),
-        "inicio"=> date('H:i', strtotime($registro['hora_inicio'])),
-        "termino" => date('H:i', strtotime($registro['hora_fim']))
-    ));
-}
+    foreach($registros as $registro){
+        array_push($atividade, array(
+            "nome_atividade" => $registro['nome_atividade'],
+            "data" => date('d-m-Y', strtotime($registro['data'])),
+            "inicio"=> date('H:i', strtotime($registro['hora_inicio'])),
+            "termino" => date('H:i', strtotime($registro['hora_fim']))
+        ));
+    }
 
-$atividades = ['atividades' => $atividade];
-$atividades = json_encode($atividades); 
+    $atividades = ['atividades' => $atividade];
+    $atividades = json_encode($atividades); 
 
-echo $atividades;
+    echo $atividades;
+
 
    
    
--- conexao.php
+++ conexao.php
@@ -4,14 +4,15 @@ class Conexao{
     private static $pdo;
 
     public static function conectar(){
-        try {
-            self::$pdo = new PDO('mysql:host=localhost;dbname=sepex;charset=utf8', 'root', '');
-            return self::$pdo;
+        if(!isset(self::$pdo)){
+            try {
+                self::$pdo = new PDO('mysql:host=localhost;dbname=sepex;charset=utf8', 'root', '');            
+            }catch (PDOException $e) {
+                print "Error: " . $e->getMessage() . "<br/>";
+                die();
+            }
         }
-        catch (PDOException $e) {
-            print "Error: " . $e->getMessage() . "<br/>";
-            die();
-        }
+        return self::$pdo;
     }
 }
 
