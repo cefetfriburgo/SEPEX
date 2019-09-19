2269ba68750e16a0f69543cf7e5367dcac93bad0
anajuliasoares <anajuliacardososoares46@gmail.com>
2019-09-19 15:35:06 -0300

    link de configuracao

--- view/admin/base/header.php
+++ view/admin/base/header.php
@@ -36,7 +36,7 @@
           <i class="fas fa-user-circle fa-fw"></i>
         </a>
         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-          <a class="dropdown-item" href="#">Configurações</a>
+          <a class="dropdown-item" href="./usuario.php">Configurações</a>
           <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Sair</a>
         </div>
       </li>
